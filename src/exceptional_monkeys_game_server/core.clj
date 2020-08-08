6173d86b8cba1a41709896cb5fb9ddf7bcc7e77f
mey.beisaron <mey.beisaron@appsflyer.com>
2020-08-09 01:18:49 +0300

    added collectables appearing on screen randomly

--- src/exceptional_monkeys_game_server/core.clj
+++ src/exceptional_monkeys_game_server/core.clj
@@ -14,21 +14,42 @@
 (def exceptionTypes ["IOException", "DivideByZeroException", "NullPointerException", "ArithmeticException", "FileNotFoundException", "IndexOutOfBoundsException",
                      "InterruptedException", "ClassNotFoundException", "NoSuchFieldException", "NoSuchMethodException", "RuntimeException"])
 
+(def collectables (atom {}))
 (def players (atom {}))
 
 (defn broadcast-msg [connections msg]
-  (doseq [con connections]
-    (async/send! (key con) (json/generate-string msg {:pretty true}))))
+  (doseq [con connections] (async/send! (key con) (json/generate-string msg {:pretty true}))))
+
+(defn init-ex [] (reset! collectables (zipmap exceptionTypes (repeat {:exception? true :show false :x 0 :y 0}))))
+
+(defn show-rand-ex []
+  (future (loop []
+            (let [max 600
+                  min 60
+                  new-val (assoc {} :exception? true  :show true :x (+ min (rand-int (- max min))) :y (+ min (rand-int (- max min))))
+                  rand-key (key (rand-nth (filter #(not (:show %)) @collectables)))]
+              (do
+                (swap! collectables assoc rand-key new-val)
+                (broadcast-msg @players (conj {:exceptionType rand-key} new-val))))
+            (Thread/sleep 5000)
+            (recur))))
+
+(defn remove-rand-ex []
+  (future (loop []
+            (let [rand-ex-key (key (rand-nth (filter #(:show %) @collectables)))]
+              (broadcast-msg @players (swap! collectables assoc-in [rand-ex-key :show] false)))
+            (Thread/sleep 5000)
+            (recur))))
 
 (defn move [con move-x move-y]
   (let [player (@players con)
         x (+ move-x (:x player))
         y (+ move-y (:y player))]
     (if (or (< y 0) (< x 0) (>= x (:windowW player)) (>= y (:windowH player)))
-      (broadcast-msg @players (assoc player :collision true)))
-    (do
-      (swap! players assoc con (assoc player :x x :y y))
-      (broadcast-msg @players (assoc player :x x :y y)))))
+      (broadcast-msg @players (assoc player :collision true))
+      (do
+        (swap! players assoc con (assoc player :x x :y y))
+        (broadcast-msg @players (assoc player :x x :y y))))))
 
 (defn handle-incoming-msg [con msg]
   (let [parsed-msg (json/parse-string msg keyword)
@@ -48,7 +69,7 @@
                     :y (rand-int 300)
                     :score 0
                     :show true
-                    :exceptionType "IoException"
+                    :exceptionType (rand-nth exceptionTypes)
                     :color [(rand-int 256) (rand-int 256) (rand-int 256)]
                     :collision false
                     :windowH 0
@@ -71,6 +92,9 @@
            (route/resources "/"))
 
 (defn -main [& {:as args}]
+  (init-ex)
+  (show-rand-ex)
+  (remove-rand-ex)
   (web/run
     (-> routes
         (web-middleware/wrap-websocket websocket-callbacks))
