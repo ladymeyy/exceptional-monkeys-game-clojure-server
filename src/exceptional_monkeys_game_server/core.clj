f46869da6dea81c65e0cf0da9fc7f805b8488202
ladymey <ladymeyy@gmail.com>
2022-07-25 13:52:58 +0300

    Better Styling, remove redundant imports

--- src/exceptional_monkeys_game_server/core.clj
+++ src/exceptional_monkeys_game_server/core.clj
@@ -1,19 +1,19 @@
 (ns exceptional-monkeys-game-server.core
-  (:require
-    [clj-uuid :as uuid]
-    [cheshire.core :as json])
-  (:use [compojure.route :only [files not-found]]
-        [compojure.core :only [defroutes GET POST DELETE ANY context]]
-        org.httpkit.server)
-  (:gen-class))
+  (:require [clj-uuid :as uuid]
+            [cheshire.core :as json]
+            [org.httpkit.server :as http-server])
+  (:use [compojure.core :only [GET]]))
 
-(def exceptionTypes ["IOException", "DivideByZeroException", "NullPointerException",  "IndexOutOfBoundsException", "InterruptedException", "RuntimeException"])
+(def exceptionTypes ["IOException", "DivideByZeroException", "NullPointerException", "IndexOutOfBoundsException"])
 (def collectables (atom {}))
 (def players (atom {}))
 
-(defn send-msg [con msg] (send! con (json/generate-string msg {:pretty true})))
+(defn send-msg [connection msg]
+  (http-server/send! connection (json/generate-string msg {:pretty true})))
 
-(defn broadcast-msg [msg] (doseq [con (keys @players)] (send-msg con msg)))
+(defn broadcast-msg [msg]
+  (doseq [connection (keys @players)]
+    (send-msg connection msg)))
 
 (defn update-collectables-game-state [collected]
   (swap! collectables dissoc (key collected))
@@ -27,28 +27,34 @@
                            :show          true
                            :exceptionType (rand-nth exceptionTypes)
                            :x             (+ min (rand-int (- max min)))
-                           :y             (+ min (rand-int (- max min)))}]
-              (swap! collectables assoc (str (uuid/v1)) new-val)
+                           :y             (+ min (rand-int (- max min)))}
+                  id (str (uuid/v1))]
+              (swap! collectables assoc id new-val)
               (broadcast-msg new-val))
             (Thread/sleep 5000)
             (recur))))
 
-(defn overlap? [playerX playerY exX exY]
-  (cond
-    (or (> playerX (+ 130 exX)) (> exX (+ 100 playerX))) false
-    (or (< (+ playerY 129) exY) (< (+ exY 200) playerY)) false
-    :else true))
+(defn is-player-overlap? [playerX playerY exX exY]
+  (not (and (or (> playerX (+ 130 exX)) (> exX (+ 100 playerX)))
+            (or (< (+ playerY 129) exY) (< (+ exY 200) playerY)))))
 
 (defn collect [player connection]
-  (let [pred (fn [[k v]] (and (= (:exceptionType v) (:exceptionType player))
-                              (overlap? (:x player) (:y player) (:x v) (:y v)))) ;calculate if player collected an item.
+  (let [pred (fn [[_ v]]
+               (and (= (:exceptionType v) (:exceptionType player))
+                    ;; calculate if player collected an item.
+                    (is-player-overlap? (:x player) (:y player) (:x v) (:y v))))
         collected (first (filter pred @collectables))]
     (if (some? collected)
       (do
-        (update-collectables-game-state collected)  ;if item was collected remove it from items map,
-        (swap! players assoc connection (assoc player :score (+ 1 (:score player)))) ;update player new score in players map
-        (assoc player :score (+ 1 (:score player))))        ; return updated player
-      player)))                                             ; else return input player
+        ;; if item was collected remove it from items map
+        (update-collectables-game-state collected)
+        ;; update player new score in players map
+        (swap! players assoc connection
+               (assoc player :score (+ 1 (:score player))))
+        ;; return updated player
+        (assoc player :score (+ 1 (:score player))))
+      ;; else return input player
+      player)))
 
 (defn move-player [player moveX moveY connection]
   (let [x (+ moveX (:x player))
@@ -56,10 +62,12 @@
     (if (or (< y 0) (< x 0) (>= x (:windowW player)) (>= y (:windowH player)))
       (assoc player :collision true)
       (do
-        (swap! players assoc connection (assoc player :x x :y y)) ;update player in players map
-        (assoc player :x x :y y)))))                        ; return updated player
+        ;; update player in players map
+        (swap! players assoc connection (assoc player :x x :y y))
+        ;; return updated player
+        (assoc player :x x :y y)))))
 
-(defn new-player [windowH windowW]
+(defn generate-new-player [windowH windowW]
   {:player?       true
    :id            (str (uuid/v1))
    :x             (rand-int 600)
@@ -67,43 +75,55 @@
    :score         0
    :show          true
    :exceptionType (rand-nth exceptionTypes)
-   :color         [(rand-int 256) (rand-int 256) (rand-int 256)]
    :collision     false
    :windowH windowH
    :windowW windowW})
 
 (defn remove-player [connection]
- (let [player (@players connection)]
-   (swap! players dissoc connection)
-   (broadcast-msg (assoc player :show false))))
+  (let [player (@players connection)]
+    (swap! players dissoc connection)
+    (broadcast-msg (assoc player :show false))))
 
 (defn add-new-player [player connection]
-  (send-msg connection (assoc player :self? true));send self to client
-  (doseq [existing-player (vals @players)] (send-msg connection existing-player));send all existing players to client
-  (doseq [existing (vals @collectables)] (send-msg connection existing)) ;send to client all existing exceptions
-  (broadcast-msg player)  ;send new player to all existing players
-  (swap! players assoc connection player)) ;add player to list.
+  ;; send self to client
+  (send-msg connection (assoc player :self? true))
+  ;; send all existing players to client
+  (doseq [existing-player (vals @players)] (send-msg connection existing-player))
+  ;; send to client all existing exceptions
+  (doseq [existing (vals @collectables)] (send-msg connection existing))
+  ;; send new player to all existing players
+  (broadcast-msg player)
+  ;; add player to list.
+  (swap! players assoc connection player))
 
 (defn update-player-state [connection newX newY]
-  (-> (move-player (@players connection) (Integer/parseInt newX) (Integer/parseInt newY) connection)
+  (-> (move-player (@players connection)
+                   (Integer/parseInt newX)
+                   (Integer/parseInt newY)
+                   connection)
       (collect connection)
       (broadcast-msg)))
 
 (defn process-message [connection message]
-  (let [json-data (json/parse-string message keyword)]
-    (if (and (:height json-data) (:width json-data))
-      (add-new-player (new-player (:height json-data) (:width json-data)) connection)
-      (update-player-state connection (:x json-data) (:y json-data)))))
+  (let [data (json/parse-string message keyword)]
+    (if (contains? @players connection)
+      (update-player-state connection (:x data) (:y data))
+      (add-new-player
+        (generate-new-player (:height data) (:width data)) connection))))
+
 
 (defn ws-handler [request]
-  (with-channel request channel
-                (on-close channel (fn [status] (println "connection closed:" status) (remove-player channel)))
-                (on-receive channel (fn [data] (process-message channel data)) )))
+  (http-server/with-channel request channel
+                            (http-server/on-close channel (fn [status]
+                                                            (println "connection closed:" status)
+                                                            (remove-player channel)))
+                            (http-server/on-receive channel (fn [data]
+                                                              (process-message channel data)))))
 
 (def websocket-routes
-  (GET "/" [] ws-handler) )
+  (GET "/" [] ws-handler))
 
 (defn -main [& {:as args}]
   (println "Starting exceptional monkeys server... ")
   (generate-rand-exception)
-  (run-server websocket-routes {:port 8080}))
+  (http-server/run-server websocket-routes {:port 8080}))
